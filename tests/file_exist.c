3f9cb1618eb992f8720580c99709c6328aaf920f
tinylcy <tinylcy@gmail.com>
2016-11-13 01:10:28 +0800

    add select and poll demo

--- tests/file_exist.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <sys/stat.h>
-
-int main(void) {
-	struct stat info;
-	char pathname[] = "../cgi-bin/myscript.sh";
-	printf("exist: %d\n", stat(pathname, &info) == 0);
-}
--- tests/is_dynamic.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main(void) {
-	char uri[] = "./cgi-bin/myscript.cgi?a=8&b=24";
-	if(strstr(uri, "cgi-bin") != NULL) {
-		printf("contains!\n");
-	}
-}
