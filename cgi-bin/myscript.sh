1cd8eb17718fd4557eb2e58d59966773e3c5cf0c
tinylcy <tinylcy@gmail.com>
2017-11-17 21:14:30 +0800

    Refactoring

--- cgi-bin/myscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf "<HTML>"
-printf "<TITLE>tinyhttpd</TITLE>"
-printf "<BODY>"
-printf "<h1>tinyhttpd - a minimum-functional HTTP Server</h1>"
-printf "<img src = '../doc/yeswedebug.jpg' height='300' width='300'/>"
-printf "</BODY>"
-printf "</HTML>"
